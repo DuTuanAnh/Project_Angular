ee475a05ce6687e00e3491b23a8290775e2b7916
Din0912 <tuananhduhocsinh2003@gmail.com>
2025-11-02 12:09:12 +0700

    gan ui test

--- Project_App/src/app/app.ts
+++ Project_App/src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TableModule } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import { Slider } from 'primeng/slider';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableModule, Slider,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  
+products: any[]=[
+{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+];
+
+value!: number;
   protected readonly title = signal('Project_App');
-}
+
+constructor() {}
+
+    ngOnInit() {
+    }
+  }
+
