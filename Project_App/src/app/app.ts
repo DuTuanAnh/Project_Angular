7f1aca38b966e8a6478f2712a47e34dccadb4802
Din0912 <tuananhduhocsinh2003@gmail.com>
2025-11-03 21:32:27 +0700

    Chinh Sua San Pham

--- Project_App/src/app/app.ts
+++ Project_App/src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { ProductService } from '../service/product.service';
+import { Product } from './domain/product';
+
+import { ImportsModule } from './imports';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TableModule,ImportsModule,CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
+  providers: [ ProductService ]
 })
-export class App {
-  protected readonly title = signal('Project_App');
+export class App implements OnInit {
+    products!: Product[];
+
+    constructor(private productService: ProductService) {}
+
+   ngOnInit() {
+        this.productService.getProductsMini().then((data) => {
+            this.products = data;
+        });
+    }
+
+    getSeverity(status: string) {
+        switch (status) {
+            case 'INSTOCK':
+                return 'success';
+            case 'LOWSTOCK':
+                return 'warn';
+            case 'OUTOFSTOCK':
+                return 'danger';
+                default:
+                    return ;
+        }
+    }
 }
