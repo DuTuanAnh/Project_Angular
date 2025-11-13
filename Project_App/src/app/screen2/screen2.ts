d2ad1f1bef8264caf8f179ac300e0ff0226e43ff
Din0912 <tuananhduhocsinh2003@gmail.com>
2025-11-13 13:16:39 +0700

    no message

---
+++ Project_App/src/app/screen2/screen2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-screen2',
+  imports: [],
+  templateUrl: './screen2.html',
+  styleUrl: './screen2.css',
+})
+export class Screen2 {
+
+}
