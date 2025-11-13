5420f408cbded0fe0b84e5c84a99ed13cb9e66ba
Din0912 <tuananhduhocsinh2003@gmail.com>
2025-11-13 13:48:03 +0700

    Hoàn tất code Screen 1

---
+++ Project_App/src/app/screen1/screen1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-screen1',
+  imports: [],
+  templateUrl: './screen1.html',
+  styleUrl: './screen1.css',
+})
+export class Screen1 {
+
+}
